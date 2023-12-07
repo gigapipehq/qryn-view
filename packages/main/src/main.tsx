37acbbfcce893364fa36b1e73b67b9ccaec4ce82
jacovinus <jacovinus@gmail.com>
2023-12-07 12:52:51 +0100

    fix basename

--- packages/main/src/main.tsx
+++ packages/main/src/main.tsx
@@ -35,7 +35,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 <React.StrictMode>
 <CookiesProvider>
     <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename="/">
             <Suspense fallback={<ScreenLoader />}>
                 <Routes>
                     <Route path="/" element={<AppRoute />}>
